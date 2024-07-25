2ebfaa534a4b98c76c394bda7c80199dcf5cb9c3
fg817sw751478 <147082067+fg817sw751478@users.noreply.github.com>
2024-07-26 00:11:08 +0200

    Updated and fixed the file i am an idiot

--- netboot.lua
+++ netboot.lua
@@ -2,7 +2,7 @@ local component = component ---@diagnostic disable-line: undefined-global
 local computer = computer ---@diagnostic disable-line: undefined-global
 
 local eeprom = component.proxy(component.list("eeprom")())
-local gpu = component.proxy(component.list("eeprom")())
+local gpu = component.proxy(component.list("gpu")())
 
 local res_x, res_y = gpu.getResolution()
 gpu.setBackground(0x003150)
@@ -12,7 +12,17 @@ gpu.fill(1, 1, res_x, res_y, " ")
 local line_count = 0
 local new_line = function(text) line_count = line_count + 1; return gpu.set(1, line_count, text) end
 
-new_line("Attempting to download Cyan BIOS from the internet...")
+function _G.reboot()
+    new_line("=> This computer will reboot in 3 seconds")
+    local time = computer.uptime()
+
+    repeat until time+3 <= computer.uptime()
+    new_line("=> Rebooting...")
+
+    computer.shutdown(true)
+end
+
+new_line("Attempting to download Blue BIOS from the internet...")
 if not component.list("internet")() then
     new_line("ERR No internet component")
     reboot()
@@ -25,14 +35,21 @@ if not internet.isHttpEnabled() then
     reboot()
 end
 
-local request = internet.request("https://github.com/OpenGCX/BlueBIOS/raw/main/binaries/blue.bin")
+local request = internet.request("https://raw.githubusercontent.com/OpenGCX/BlueBIOS/main/binaries/blue.bin")
 
 if request then
     new_line("SUCCESS Fetched data")
-    local data = request.read()
+    local data = ""
+    local chunk
+    ::parse::
+    chunk = request.read()
+    if chunk then
+        data = data .. chunk
+        goto parse
+    end
     if data then
         new_line("Attempting to flash data...")
-        local result, reason = pcall(component.invoke(eeprom.address, "set", data))
+        local result, reason = pcall(component.invoke, eeprom.address, "set", data)
         if not result then
             new_line("ERR " .. tostring(reason))
             reboot()
@@ -46,13 +63,3 @@ if request then
 end
 
 reboot()
-
-function _G.reboot()
-    new_line("=> This computer will reboot in 3 seconds")
-    local time = computer.uptime()
-
-    repeat until time+3 <= computer.uptime()
-    new_line("=> Rebooting...")
-
-    computer.shutdown(true)
-end
